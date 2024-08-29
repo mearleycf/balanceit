a26f8aca1f195e20c22aa9090d8ec6d79de9d5fa
Mike Earley <mike@5sn1.com>
2024-08-29 14:20:29 -0400

    refactor: adding @astro/svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,15 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap({
-      changeFreq: 'weekly',
-      priority: 0.8,
-      lastmod: new Date(),
-    }),
-    icon(),
-  ],
+  integrations: [tailwind(), sitemap({
+    changeFreq: 'weekly',
+    priority: 0.8,
+    lastmod: new Date(),
+  }), icon(), svelte()],
   output: 'server',
   adapter: vercel(),
   image: {
